336a6afd4c365da526125027b2a9e8b436d7ea40
Satyam Kumar <ksatyam433@gmail.com>
2022-12-27 17:01:57 +0530

    Displaying the summary logic done

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ const displayMovements = function (movements) {
         const html = `
                 <div class="movements__row">
                     <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
-                    <div class="movements__value">${mov}</div>
+                    <div class="movements__value">₹ ${mov}</div>
                 </div>`;
 
         containerMovements.insertAdjacentHTML('afterbegin', html);
@@ -75,6 +75,32 @@ const displayMovements = function (movements) {
 };
 
 displayMovements(account1.movements);
+
+const calcDisplayBalance = function (movements) {
+    const balance = movements.reduce((acc, mov) => acc + mov, 0);
+    labelBalance.textContent = `₹ ${balance}`;
+};
+calcDisplayBalance(account1.movements);
+
+const calcDisplaySummary = function (movements) {
+    const incomes = movements.filter((mov) => mov > 0).reduce((acc, mov) => acc + mov, 0);
+    labelSumIn.textContent = `₹ ${incomes}`;
+
+    const outcomes = movements.filter((mov) => mov < 0).reduce((acc, mov) => acc + mov, 0);
+    labelSumOut.textContent = `₹ ${Math.abs(outcomes)}`;
+
+    const interest = movements
+        .filter((mov) => mov > 0)
+        .map((deposit) => (deposit * 1.2) / 100)
+        .filter((int, i, arr) => {
+            console.log(arr);
+            return int >= 1;
+        })
+        .reduce((acc, int) => acc + int, 0);
+    labelSumInterest.textContent = `₹ ${interest}`;
+};
+
+calcDisplaySummary(account1.movements);
 
 const createUserNames = function (accs) {
     accs.forEach(function (acc) {
@@ -86,4 +112,3 @@ const createUserNames = function (accs) {
     });
 };
 createUserNames(accounts);
-console.log(accounts);
