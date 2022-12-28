abf30171f0821867bf10d32b12fbfa66e38f7257
Satyam Kumar <ksatyam433@gmail.com>
2022-12-29 01:36:03 +0530

    Date and time logic modified

--- script.js
+++ script.js
@@ -13,10 +13,10 @@ const account1 = {
         '2019-12-23T07:42:02.383Z',
         '2020-01-28T09:15:04.904Z',
         '2020-04-01T10:17:24.185Z',
-        '2020-05-08T14:11:59.604Z',
-        '2020-05-27T17:01:17.194Z',
-        '2020-07-11T23:36:17.929Z',
-        '2020-07-12T10:51:36.790Z',
+        '2023-12-24T14:11:59.604Z',
+        '2022-12-25T17:01:17.194Z',
+        '2022-12-27T23:36:17.929Z',
+        '2022-12-28T10:51:36.790Z',
     ],
     currency: 'EUR',
     locale: 'pt-PT', // de-DE
@@ -74,6 +74,20 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // Functions
 
+const formatMovementDate = function (date) {
+    const calcDaysPassed = (date1, date2) => Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
+    const daysPassed = calcDaysPassed(new Date(), date);
+    if (daysPassed === 0) return 'Today';
+    if (daysPassed === 1) return 'Yesterday';
+    if (daysPassed <= 7) return `${daysPassed} days ago..`;
+    else {
+        const day = `${date.getDate()}`.padStart(2, 0);
+        const month = `${date.getMonth() + 1}`.padStart(2, 0);
+        const year = date.getFullYear();
+        return `${day}/${month}/${year}`;
+    }
+};
+
 const displayMovements = function (acc, sort = false) {
     containerMovements.innerHTML = '';
 
@@ -81,12 +95,9 @@ const displayMovements = function (acc, sort = false) {
 
     movs.forEach(function (mov, i) {
         const type = mov > 0 ? 'deposit' : 'withdrawal';
-
         const date = new Date(acc.movementsDates[i]);
-        const day = `${date.getDate()}`.padStart(2, 0);
-        const month = `${date.getMonth() + 1}`.padStart(2, 0);
-        const year = date.getFullYear();
-        const displayDate = `${day}/${month}/${year}`;
+
+        const displayDate = formatMovementDate(date);
 
         const html = `
       <div class="movements__row">
@@ -224,6 +235,8 @@ btnLoan.addEventListener('click', function (e) {
 
         // Update UI
         updateUI(currentAccount);
+        // Highlighting the alternate rows
+        highlight();
     }
     inputLoanAmount.value = '';
 });
@@ -256,6 +269,6 @@ btnSort.addEventListener('click', function (e) {
 // Function to highlight the alternate rows
 const highlight = function () {
     [...document.querySelectorAll('.movements__row')].forEach(function (row, i) {
-        if (i % 2 === 0) row.style.backgroundColor = '#f1f1f1';
+        if (i % 2 !== 0) row.style.backgroundColor = '#f1f1f1';
     });
 };
